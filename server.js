eb8c999c0e8d9076f5adf6567c845bf9f6b2ceab
Alex Tapia <realalextapia@gmail.com>
2023-04-01 19:09:41 -0400

    added routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8081
 const cors = require("cors");
 const { CORS_ORIGIN } = process.env;
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.static('public'))
 
 app.use(cors({origin:CORS_ORIGIN}));
