23700b05a5d2e5c7eff2d6c5528ccda12620e543
Alex Tapia <realalextapia@gmail.com>
2023-04-03 17:27:24 -0400

    update routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = express();
 const express = require("express");
+const app = express();
 
 require('dotenv').config();
 const PORT = process.env.PORT || 8081
@@ -12,8 +12,15 @@ app.use(express.static('public'))
 
 app.use(cors({origin:CORS_ORIGIN}));
 
+// const warehousesRoute = require('./routes/warehouseRoute');
+// app.use('/warehouses', warehousesRoute);
 
+const dateRoute = require('./routes/dateRoute')
+app.use('/dates', dateRoute);
 
+app.get('/', (req, res) => {
+    console.log('connected to / route')
+})
 
 app.listen(PORT, () =>{
     console.log("listening on port 8080")
