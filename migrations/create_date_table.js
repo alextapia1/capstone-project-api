f64065092d8ced6e2957bf13f08757db4b414154
Alex Tapia <realalextapia@gmail.com>
2023-04-07 23:37:02 -0400

    refactor table

--- migrations/create_date_table.js
+++ migrations/create_date_table.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
     return knex.schema.createTable('dates', (table) =>{
         table.uuid('item_id').primary();
         table.string('info').notNullable();
+        table.string('category').notNullable();
         table.boolean('complete').defaultTo(false)
     });
 };
--- seeds/01_dates.js
+++ seeds/01_dates.js
@@ -4,16 +4,19 @@ exports.seed = async function (knex) {
         {
             item_id: '1',
             info: 'meeting',
+            category:'notes',
             complete: false
         },
         {
             item_id: '2',
             info: 'meeting',
+            category: 'grocery list',
             complete: true
         },
         {
             item_id: '3',
             info: 'meeting',
+            category:'pending',
             complete: true
         },
     ]);
